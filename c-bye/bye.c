dce53393d092e7b7ee1a7e824398c64b6b278861
Sebastian Ionita <sebi.ionita23@gmail.com>
2026-07-02 10:59:31 +0300

    Add CI workflow
    Signed-off-by: Sebastian Ionita <sebi.ionita23@gmail.com>

--- c-bye/bye.c
+++ c-bye/bye.c
@@ -4,18 +4,15 @@
 char *password = "n0t_saf3_4nyw4y";
 
 int main() {
+  char input[100];
+  fgets(input, sizeof(input), stdin);
+  input[strcspn(input, "\n")] = '\0';
 
-    char input[100];
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = '\0';
+  if (strcmp(input, password) == 0) {
+    printf("Access granted!\nBye bye!\n");
+  } else {
+    printf("Access denied!\nNo goodbye for you!\n");
+  }
 
-    if (strcmp(input, password) == 0) {
-        printf("Access granted!\nBye bye!\n");
-    } else {
-        printf("Access denied!\nNo goodbye for you!\n");
-    }
-
-    return 0;              
+  return 0;
 }
-
-
